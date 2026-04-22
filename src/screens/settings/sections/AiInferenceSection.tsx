579082a9c72660c4f241c57ac016148a98c22d7c
vgn96186 <vgn96186@gmail.com>
2026-04-22 15:53:29 +0530

    remove orphaned AiInferenceSection

--- src/screens/settings/sections/AiInferenceSection.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { View } from 'react-native';
-import AiProvidersSection from './AiProvidersSection';
-
-export function AiInferenceSection(props: any) {
-  return <AiProvidersSection {...props} />;
-}

dbba52994f482fb79dde4a95178577294d97d238
vgn96186 <vgn96186@gmail.com>
2026-04-28 19:48:03 +0530

    recover(storage): add MMKV backing store

---
+++ __mocks__/react-native-mmkv.js
@@ -0,0 +1,10 @@
+module.exports = {
+  createMMKV: () => ({
+    getString: () => undefined,
+    getBoolean: () => undefined,
+    getNumber: () => undefined,
+    set: () => undefined,
+    delete: () => undefined,
+    clearAll: () => undefined,
+  }),
+};
